2c2e7395c8ad010528121a1e3348487ea57df8db
Omer-5 <OmerHazan5@gmail.com>
2022-05-20 16:12:49 +0300

    screen size and secondary color

--- Damka/Classes/Constants.cs
+++ Damka/Classes/Constants.cs
@@ -9,13 +9,14 @@ namespace Damka.Classes
     class Constants
     {
         // UI Elements
-        public const int SCREEN_SIZE_WIDTH = 2560;
-        public const int SCREEN_SIZE_HEIGHT = 1440;
+        public const int SCREEN_SIZE_WIDTH = 1920;
+        public const int SCREEN_SIZE_HEIGHT = 1080;
         public const int PANEL_SIZE = 720;
         public const int BUTTON_SIZE = 90;
         public static Color LIGHT_BROWN = System.Drawing.Color.FromArgb(66, 43, 34); // being set at Damka.cs - it's a duplicate
         public static Color DARK_BROWN = System.Drawing.Color.FromArgb(113, 82, 60); // the same
         public static Color selectedColor = System.Drawing.Color.FromArgb(226, 233, 233);
+        public static Color selectedColorSecondary = System.Drawing.Color.FromArgb(245, 144, 2);
 
         public const int NUM_OF_ROWS = 8;
         public const int NUM_OF_COLS = 8;
